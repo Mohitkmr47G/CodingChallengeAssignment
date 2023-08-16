29e5446d030e722efa8b73251d97b8f3d7e0d593
MohitKumar <120587670+Mohitkmr47G@users.noreply.github.com>
2023-08-16 23:17:23 +0530

    Create Q4.js

---
+++ Q4.js
@@ -0,0 +1,16 @@
+const books = [
+  { title: "Book 1", author: "author 1", year: 2005 },
+  { title: "Book 2", author: "author 2", year: 2012 },
+  { title: "Book 3", author: "author 3", year: 2008 },
+  { title: "Book 4", author: "author 4", year: 2015 },
+  { title: "Book 5", author: "author 5", year: 2003 },
+];
+
+const filteredAndCapitalized = books
+  .filter(book => book.year >= 2010)
+  .map(book => ({
+    title: book.title,
+    author: book.author.toUpperCase(),
+  }));
+
+console.log("Filtered and Capitalized Books:", filteredAndCapitalized);
